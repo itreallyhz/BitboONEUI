4607d8abd3a43eab41a19b612c5553aad2a2de80
itreallyhz <mikapmarquez@gmail.com>
2024-01-26 09:51:53 +0800

    update wsgi

--- BITBO_ONEPAGE/wsgi.py
+++ BITBO_ONEPAGE/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for iskonect project.
+WSGI config for bitbooneui project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iskonect.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bitbooneui.settings')
 
 application = get_wsgi_application()
